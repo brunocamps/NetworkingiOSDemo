69d66cc976bcce5c6b3c07e4ce8a53d648b536d4
brunocamps <bruno.mcamps@outlook.com>
2023-05-31 10:07:17 -0300

    Initial Commit

---
+++ NetworkingiOSDemo/NetworkingiOSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingiOSDemoApp.swift
+//  NetworkingiOSDemo
+//
+//  Created by Bruno Campos on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingiOSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
